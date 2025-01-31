825f43130a957b4a9d27769838651da89a582837
Lawice <129510417+Lawice@users.noreply.github.com>
2025-01-31 16:37:11 +0100

    Despawn

--- Assets/Script/Runtime/Objects/IObjectVisibility.cs
+++
@@ -1,8 +0,0 @@
-using Unity.Netcode;
-using UnityEngine;
-
-public interface IObjectVisibility
-{
-    [ServerRpc]
-    public void ToogleVisibilityServerRpc(bool state);
-}
--- Assets/Script/Runtime/Player/ScPlayerInteraction.cs
+++ Assets/Script/Runtime/Player/ScPlayerInteraction.cs
@@ -5,35 +5,35 @@ using Unity.Netcode;
 
 namespace Dindio.Runtime.Player {
 
-    public class ScPlayerInteraction : NetworkBehaviour
-    {
+    public class ScPlayerInteraction : NetworkBehaviour {
         ScInputManager _inputManager => ScInputManager.Instance;
-        [SerializeField]private float _detectionRadius;
+        [SerializeField] private float _detectionRadius;
         ScPlayerInventory _playerInventory;
-        void Start() 
-        {
+        void Start() {
             _inputManager.OnInteractEvent.Performed.AddListener(TryInteract);
             _playerInventory = GetComponent<ScPlayerInventory>();
         }
 
-        void TryInteract()
-        {
+        void TryInteract() {
             Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, _detectionRadius);
-            foreach (Collider2D collider in colliders)
-            {
-                if(collider.gameObject.TryGetComponent(out ScInteractable interactable))
-                {
-                    interactable.Interact(_playerInventory);
-                    ToogleVisibilityServerRpc(collider.gameObject, false);
-                    break;
-                }
+            foreach (Collider2D collider in colliders) {
+                if (!collider.gameObject.TryGetComponent(out ScInteractable interactable)) continue;
+                interactable.Interact(_playerInventory);
+                DespawnObjectServerRpc(collider.gameObject.GetComponent<NetworkObject>());
+                break;
             }
         }
 
         [ServerRpc(RequireOwnership = false)]
-        public void ToogleVisibilityServerRpc(GameObject go ,bool state)
-        {
-            go.GetComponent<NetworkObject>().Despawn();
+        private void DespawnObjectServerRpc(NetworkObjectReference objref) {
+            DespawnObjectClientRpc(objref);
+        }
+
+        [ClientRpc]
+        private void DespawnObjectClientRpc(NetworkObjectReference objref) {
+            if (objref.TryGet(out NetworkObject obj)) {
+                obj.Despawn();
+            }
         }
     }
 
