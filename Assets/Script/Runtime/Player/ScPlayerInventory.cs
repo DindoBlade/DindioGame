24aa1beca9ea51c3d426900579a9bbdce2bf0fea
Gabibel15022005 <153726789+Gabibel15022005@users.noreply.github.com>
2025-02-03 12:28:39 +0100

    ToolBar

--- Assets/Script/Runtime/Player/ScPlayerInventory.cs
+++ Assets/Script/Runtime/Player/ScPlayerInventory.cs
@@ -1,9 +1,11 @@
 using System.Collections.Generic;
 using Dindio.Runtime.Interactable.Inventory;
+using Unity.VisualScripting;
 using UnityEngine;
+using Unity.Netcode;
 
 namespace Dindio.Runtime.Player {
-    public class ScPlayerInventory : MonoBehaviour {
+    public class ScPlayerInventory : NetworkBehaviour {
         [SerializeField] List<SoInventoryItem> _items = new(new SoInventoryItem[5]);
         private int _itemIDSelected;
 
@@ -13,15 +15,27 @@ namespace Dindio.Runtime.Player {
             }
 
             if (_items[_itemIDSelected]) {
+                Debug.Log("Is Called");
                 DropInventory(_items[_itemIDSelected]);
             }
 
             _items[_itemIDSelected] = item;
+
         }
 
+        
         public void DropInventory(SoInventoryItem item)
         {
-            //spawn la prefab
+            GameObject objectToSpawn = Instantiate(item.Prefab, transform.position, Quaternion.identity);
+            SpawnServerRpc(objectToSpawn.GetComponent<NetworkObject>());
+        }
+
+        [ServerRpc(RequireOwnership = false)]
+        public void SpawnServerRpc(NetworkObjectReference objectRef)
+        {
+            if (objectRef.TryGet(out NetworkObject obj)) {
+                obj.Spawn();
+            }
         }
     }
 }
