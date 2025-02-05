29a3a049d3bb246161e374340be27304a5ba9dd3
takuryto2 <louisguichard761@gmail.com>
2025-02-05 09:51:02 +0100

    edit sur les anciennes préfabs + début kill feed

---
+++ Assets/Script/Runtime/ScKillFeed.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ScKillFeed : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
