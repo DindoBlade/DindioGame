1100584dc53d1ff67446f7dfbc1e79d4e07067f3
Gabibel15022005 <153726789+Gabibel15022005@users.noreply.github.com>
2025-02-13 12:58:48 +0100

    minor change
    scale of the map
    crate prefab
    Added tag to playerPrefab
    Change in some script

--- Assets/Script/Runtime/Interactable/ScCrateHealth.cs
+++ Assets/Script/Runtime/Interactable/ScCrateHealth.cs
@@ -1,4 +1,5 @@
-﻿using Dindio.Runtime.Interfaces;
+﻿using System.Collections;
+using Dindio.Runtime.Interfaces;
 using Unity.Netcode;
 using UnityEngine;
 namespace Dindio.Runtime.Interactable {
@@ -8,23 +9,52 @@ namespace Dindio.Runtime.Interactable {
         [field: SerializeField] public int MaxHp { get; set; }
         public NetworkVariable<int> CurrentHp { get; set; }  = new ();
         private ScCrateSpawnItem _spawner;
+        [SerializeField] private GameObject _visuel;
+        [SerializeField] private float _timeUntilRespawn;
+
 
         private void Start() {
             _spawner = GetComponent<ScCrateSpawnItem>();
         }
         public void TakeDamage(int amount)
         {
+
+            _visuel.SetActive(false);
+
             _spawner.SpawnItemServerRpc();
             DestroyServerRpc();
+
         }
 
 
         [ServerRpc(RequireOwnership = false)]
         private void DestroyServerRpc() {
+
             Debug.Log("Destroyed");
+
             if (transform.TryGetComponent(out NetworkObject obj)) {
-                obj.Despawn();
+                obj.GetComponent<ScCrateHealth>()._visuel.SetActive(false);
+                StartCoroutine(Respawn());
             }
         }
+
+        [ClientRpc(RequireOwnership = false)]
+        private void RespawnClientRpc()
+        {
+            _visuel.SetActive(true);
+        }
+
+
+        private IEnumerator Respawn() {
+
+            yield return new WaitForSeconds(_timeUntilRespawn);
+
+            if (transform.TryGetComponent(out NetworkObject obj)) {
+                RespawnClientRpc();
+                //obj.Despawn();
+            }
+        }
+
+
     }
 }
