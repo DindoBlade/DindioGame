b3b95ba012aa5883ea6e2d568bce286460dd811c
Lawice <129510417+Lawice@users.noreply.github.com>
2025-02-14 10:06:31 +0100

    Final things

--- Assets/Script/Runtime/UI/MainMenu/ScMainMenu.cs
+++ Assets/Script/Runtime/UI/MainMenu/ScMainMenu.cs
@@ -3,8 +3,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ScMainMenu : MonoBehaviour {
+
+    [SerializeField] private GameObject _mainMenuUI;
+    [SerializeField] private GameObject _leaderboardMenuUI;
+    
     public void Play() {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         // NetworkManager.Singleton.StartClient();
+    }
+
+    public void OpenLeaderboard() {
+        _mainMenuUI.SetActive(false);
+        _leaderboardMenuUI.SetActive(true);
+    }
+    
+    public void CloseLeaderboard() {
+        _mainMenuUI.SetActive(true);
+        _leaderboardMenuUI.SetActive(false);
     }
 }
